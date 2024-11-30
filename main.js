f3e5960078df1f75194b27fc56be4776fb3fb969
TheSmith0803 <ethan.m.smith0803@gmail.com>
2024-11-30 10:41:44 -0500

    JS notes

---
+++ main.js
@@ -0,0 +1,18 @@
+let namew = "John";
+let surname = "Doe";
+
+console.log(namew);
+console.log(surname);
+
+
+// ressainging variables
+let age = 11;
+console.log(age);
+
+/** You do not need to use the 
+ * let keyword again to redefine
+ * an already declared variable.
+ */
+age = 54;
+
+console.log(age);
